657200f1e72df1c2ea2035c4073ca031b135de67
Paula Vun <paulavun@Paulas-MacBook-Pro.local>
2019-02-24 19:53:48 -0800

    added else to alert an error message

--- js/scripts.js
+++ js/scripts.js
@@ -12,28 +12,31 @@ $(document).ready(function() {
          if (level === 'easy' || level ==='active' || level ==='beyond')
           if (prefer === 'seperate' || prefer === 'group' || prefer === 'guided'){
             $("#disney").show()
-          }
-          if (person === 'solo' || person === 'child' || person === 'adult')
-            if (trip === 'weekend' || trip === 'holiday')
-              if (activity === 'nightlife')
-                if (level === 'easy' || level ==='active' || level ==='beyond')
-                  if (prefer === 'seperate' || prefer === 'group' || prefer === 'guided'){
-                    $("#vegas").show()
-          }
-          if (person === 'solo' || person === 'child' || person === 'adult')
-            if (trip === 'weekend' || trip === 'holiday' || trip === 'camping')
-              if (activity === 'beach')
-                if (level === 'easy' || level ==='active' || level ==='beyond')
-                  if (prefer === 'seperate' || prefer === 'group' || prefer === 'guided'){
-                    $("#hawaii").show()
-          }
-          if (person === 'solo' || person === 'child' || person === 'adult')
-            if (trip === 'weekend' || trip === 'holiday' || trip === 'camping')
-              if (activity === 'sightseeing')
-                if (level === 'easy' || level ==='active' || level ==='beyond')
-                  if (prefer === 'seperate' || prefer === 'group' || prefer === 'guided'){
-                    $("#grandcanyon").show()
-          }
+    }
+    if (person === 'solo' || person === 'child' || person === 'adult')
+      if (trip === 'weekend' || trip === 'holiday')
+        if (activity === 'nightlife')
+          if (level === 'easy' || level ==='active' || level ==='beyond')
+            if (prefer === 'seperate' || prefer === 'group' || prefer === 'guided'){
+              $("#vegas").show()
+    }
+    if (person === 'solo' || person === 'child' || person === 'adult')
+      if (trip === 'weekend' || trip === 'holiday' || trip === 'camping')
+        if (activity === 'beach')
+          if (level === 'easy' || level ==='active' || level ==='beyond')
+            if (prefer === 'seperate' || prefer === 'group' || prefer === 'guided'){
+              $("#hawaii").show()
+    }
+    if (person === 'solo' || person === 'child' || person === 'adult')
+      if (trip === 'weekend' || trip === 'holiday' || trip === 'camping')
+        if (activity === 'sightseeing')
+          if (level === 'easy' || level ==='active' || level ==='beyond')
+            if (prefer === 'seperate' || prefer === 'group' || prefer === 'guided'){
+              $("#grandcanyon").show()
+    } else {
+      alert("OOOOPPSSS! There was an error. Please try again");
+    }
+    
     event.preventDefault();
 
   });
