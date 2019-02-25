0ed659088e9f548be5fb04e404279d65b245b164
Paula Vun <paulavun@Paulas-MacBook-Pro.local>
2019-02-24 19:05:01 -0800

    added branching to show hawaii.

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,13 @@ $(document).ready(function() {
                   if (prefer === 'seperate' || prefer === 'group' || prefer === 'guided'){
                     $("#vegas").show()
           }
+          if (person === 'solo' || person === 'child' || person === 'adult')
+            if (trip === 'weekend' || trip === 'holiday' || trip === 'camping')
+              if (activity === 'beach')
+                if (level === 'easy' || level ==='active' || level ==='beyond')
+                  if (prefer === 'seperate' || prefer === 'group' || prefer === 'guided'){
+                    $("#hawaii").show()
+          }
 
     event.preventDefault();
 
